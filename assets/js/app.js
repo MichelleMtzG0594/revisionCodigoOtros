f8ce5a973705028f6deada57f1010e478d42415c
Michelle Mtz <michellemtzg.proyectos@gmail.com>
2025-05-26 11:30:17 -0600

    add codigo de revision

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('.name'); 
+/* Cambio de # a . ya que es una clase */
+const $b = document.querySelector('.blog'); 
+const $l = document.querySelector('.location'); 
+
+/*   function displayUser (username){
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
+} */
+
+
+const displayUser = async (data) => { /* Faltaba el async */
+  try { /* Aqui mismo pue el try y catch para no tener 2 funciones distintas */
+    $n.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${data}`);
+    console.log(data);
+    $n.textContent = `${data.name}`; /* Cambiar a backticks y renombre parametro de username a data */
+    $n.textContent = `${data.blog}`; /* Cambiar a backticks */
+    $l.textContent = `${data.location}`; /* Cambiar a backticks */
+  } catch (error) { 
+    console.log('OH NO!');
+    console.log(error);
+    $n.textContent = `Algo salió mal: ${error}`
+  }
+
+}
+
+displayUser('stolinski')
