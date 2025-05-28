9d6a9824c1b6d239d50dab7ab2e38dfe918bd874
Michelle Mtz <michellemtzg.proyectos@gmail.com>
2025-05-28 16:52:39 -0600

    Feat: ejercicio 2 de revison de código

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,10 +19,13 @@ const displayUser = async (data) => { /* Faltaba el async */
   try { /* Aqui mismo pue el try y catch para no tener 2 funciones distintas */
     $n.textContent = 'cargando...';
     const response = await fetch(`${usersEndpoint}/${data}`);
+    const datosApi = await response.json();
+   
     console.log(data);
     $n.textContent = `${data.name}`; /* Cambiar a backticks y renombre parametro de username a data */
     $n.textContent = `${data.blog}`; /* Cambiar a backticks */
     $l.textContent = `${data.location}`; /* Cambiar a backticks */
+         return datosApi;
   } catch (error) { 
     console.log('OH NO!');
     console.log(error);
